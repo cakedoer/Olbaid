495b6ec868b6084ba2db7c6855824bd441b939d7
cakedoer <cakedoer@gmail.com>
2026-06-02 21:27:23 +0300

    Intial commit basic main menu

---
+++ Olbaid/Models/SimpleMenu.cs
@@ -0,0 +1,61 @@
+﻿namespace Olbaid.Models;
+
+public class SimpleMenu(string menuTitle, string[] options)
+{
+    private int _selectedIndex = 0;
+    
+    public T ShowAndGetSelection<T>() where T : struct, Enum
+    {
+        var values = Enum.GetValues<T>();
+        
+        bool selecting = true;
+        while (selecting)
+        {
+            Render();
+            
+            // only has up and down for now
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    _selectedIndex = (_selectedIndex == 0) ? options.Length - 1 : _selectedIndex - 1;
+                    break;
+                case ConsoleKey.DownArrow:
+                    _selectedIndex = (_selectedIndex == options.Length - 1) ? 0 : _selectedIndex + 1;
+                    break;
+                case ConsoleKey.Enter:
+                    selecting = false;
+                    break;
+            }
+        }
+
+        return values[_selectedIndex];
+    }
+
+    private void Render()
+    {
+        // Console.SetCursorPosition(0, 0); // leaves artefacts
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"=== {menuTitle.ToUpper()} ===");
+        Console.ResetColor();
+        Console.WriteLine("Use Up/Down Arrow keys and press Enter:\n");
+
+        for (int i = 0; i < options.Length; i++)
+        {
+            if (i == _selectedIndex)
+            {
+                Console.Write(" > ");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write($" {options[i]} ");
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine($"    {options[i]}");
+            }
+        }
+    }
+}
---
+++ Olbaid/Program.cs
@@ -0,0 +1,72 @@
+﻿using Olbaid.Models;
+
+namespace Olbaid;
+
+public enum GameState
+{
+    Mainmenu,
+    Newgame,
+    Loadgame,
+    Quit
+}
+
+public enum MainMenuOption
+{
+    Newgame,
+    Loadgame,
+    Quit
+}
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.CursorVisible = false;
+        bool quit = false;
+        GameState gameState = GameState.Mainmenu;
+
+        // define menus like this for now, todo composite pattern menus for point allocation etc.
+        SimpleMenu mainMenu = new SimpleMenu("Main Menu", ["New Game", "Load Game", "Quit"]);
+            
+        while (!quit)
+        {
+            switch (gameState)
+            {
+                case GameState.Mainmenu:
+                    MainMenuOption mainSelection = mainMenu.ShowAndGetSelection<MainMenuOption>();
+                    gameState = mainSelection switch
+                    {
+                        MainMenuOption.Newgame  => GameState.Newgame,
+                        MainMenuOption.Loadgame => GameState.Loadgame,
+                        MainMenuOption.Quit     => GameState.Quit,
+                        _                       => gameState
+                    };
+                    break;
+
+                case GameState.Newgame:
+                    // todo game logic
+                    Console.Clear();
+                    Console.WriteLine("\nPress any key to continue...");
+                        
+                    Console.ReadKey(true); 
+                        
+                    gameState = GameState.Mainmenu; 
+                    break;
+                    
+                case GameState.Loadgame:
+                    Console.Clear();
+                    Console.WriteLine("\nPress any key to continue...");
+                        
+                    Console.ReadKey(true); 
+                        
+                    gameState = GameState.Mainmenu; 
+                    break;
+
+                case GameState.Quit:
+                    Console.CursorVisible = true;
+                    quit = true;
+                    break;
+            }
+        }
+    }
+}
